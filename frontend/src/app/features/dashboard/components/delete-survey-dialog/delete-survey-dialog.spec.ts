63031ac35233686739219335bd27e5dbf2b599ea
Jakub Bryła <251130@edu.p.lodz.pl>
2026-06-02 13:30:32 +0200

    Poprawki od CodeRabbit

--- frontend/src/app/features/dashboard/components/delete-survey-dialog/delete-survey-dialog.spec.ts
+++ frontend/src/app/features/dashboard/components/delete-survey-dialog/delete-survey-dialog.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
++import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { DeleteSurveyDialog } from './delete-survey-dialog';
 
@@ -9,6 +10,12 @@ describe('DeleteSurveyDialog', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DeleteSurveyDialog],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {title: 'Testowa ankieta'}
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DeleteSurveyDialog);
