6896f0e13d17f6ba1b6e0d19a8f14ea7fa3c3d16
Jakub Bryła <251130@edu.p.lodz.pl>
2026-06-02 16:39:12 +0200

    Dodanie opcji zmianu statusu ankiety i przycisku udostępnienia

--- frontend/src/app/features/dashboard/components/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/components/dashboard/dashboard.ts
@@ -1,11 +1,22 @@
 import { Component, inject, ChangeDetectorRef } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatChipsModule } from '@angular/material/chips';
 import { RouterModule } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { CreateSurveyDialog } from '../create-survey-dialog/create-survey-dialog';
 import { DeleteSurveyDialog } from '../delete-survey-dialog/delete-survey-dialog';
+
+export interface SurveySummary {
+  id: number;
+  title: string;
+  description: string;
+  status: 'draft' | 'active' | 'closed';
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -13,20 +24,34 @@ import { DeleteSurveyDialog } from '../delete-survey-dialog/delete-survey-dialog
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    RouterModule
+    MatMenuModule,
+    MatChipsModule,
+    RouterModule,
+    NgClass,
+    MatSnackBarModule
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+  private snackBar = inject(MatSnackBar);
   private dialog = inject(MatDialog);
   private cdr = inject(ChangeDetectorRef);
   // mock baza danych ankiet na potrzeby frontendu
-  surveys = [
-    { id: 1, title: 'Ankieta satysfakcji klienta', status: 'Aktywna', responses: 15 },
-    { id: 2, title: 'Badanie rynku IT 2026', status: 'Zakończona', responses: 142 },
-    { id: 3, title: 'Ocena szkolenia', status: 'Szkic', responses: 0 }
+  surveys: SurveySummary[] = [
+    { id: 1, title: 'Badanie satysfakcji z pracy w IT', description: 'Anonimowa ankieta dla programistów.', status: 'draft' },
+    { id: 2, title: 'Ulubione frameworki 2026', description: 'Głosowanie na najlepsze narzędzia.', status: 'active' },
+    { id: 3, title: 'Zapotrzebowanie na szkolenia', description: 'Ankieta wewnętrzna dla firmy.', status: 'closed' }
   ];
+
+  changeSurveyStatus(survey: SurveySummary, newStatus: 'draft' | 'active' | 'closed') {
+    survey.status = newStatus;
+    
+    console.log(`AKTUALIZACJA STATUSU`);
+    console.log(`Ankieta ID: ${survey.id} zmieniła status na: ${newStatus}`);
+    
+    // W przyszłościw wywołać API do aktualizacji statusu ankiety w backendzie
+  }
 
   openCreateSurveyDialog() {
     const dialogRef = this.dialog.open(CreateSurveyDialog, {
@@ -44,8 +69,8 @@ export class Dashboard {
           {
             id: newId,
             title: result,
-            status: 'Szkic',
-            responses: 0
+            description: '',
+            status: 'draft'
           }
         ];
         this.cdr.detectChanges();
@@ -73,6 +98,20 @@ export class Dashboard {
         
         console.log(`Ankieta "${surveyTitle}" została usunięta!`);
       }
+    });
+  }
+
+  shareSurvey(surveyId: number) {
+    const url = `${window.location.origin}/s/${surveyId}`;
+    
+    navigator.clipboard.writeText(url).then(() => {
+      this.snackBar.open('🔗 Link skopiowany do schowka!', 'OK', {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom'
+      });
+    }).catch(err => {
+      console.error('Błąd podczas kopiowania linku: ', err);
     });
   }
 }
