d6b59b47498e22cfea06f14240426f0a26943c07
Jakub Bryła <251130@edu.p.lodz.pl>
2026-06-02 11:08:53 +0200

    Integracja formularzy z serwisem do autentykacji i dodanie logiki wysyłania

--- frontend/src/app/features/auth/components/login/login.ts
+++ frontend/src/app/features/auth/components/login/login.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  private auth = inject(Auth);
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const credentials = this.loginForm.value;
+      
+      this.auth.login(credentials);
+      
+      console.log('Komponent Logowania: Przekazałem dane do serwisu!');
+    } else {
+      console.log('Formularz jest niepoprawny.');
+    }
+  }
+}
