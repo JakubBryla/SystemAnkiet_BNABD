bfe6a24a3fb40c188bb2c3a17c01fd76fb9a17ae
Jakub Bryła <251130@edu.p.lodz.pl>
2026-06-09 16:08:17 +0200

    Poprawa rejestracji i logowania

--- frontend/src/app/features/auth/components/login/login.ts
+++ frontend/src/app/features/auth/components/login/login.ts
@@ -1,15 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
@@ -21,11 +22,18 @@ export class Login {
     password: new FormControl('', [Validators.required])
   });
 
+  serverError = signal<string | null>(null);
+
   onSubmit() {
     if (this.loginForm.valid) {
+      this.serverError.set(null);
       const credentials = this.loginForm.value;
 
+      // Wywołanie serwisu Auth
       this.auth.login(credentials);
+      
+      // błąd logowania do testu
+      // this.serverError.set('Nieprawidłowy adres e-mail lub hasło.');
     }
   }
 }
--- frontend/src/app/features/auth/components/register/register.ts
+++ frontend/src/app/features/auth/components/register/register.ts
@@ -1,11 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
-
+import { MatIconModule } from '@angular/material/icon';
 import { Auth } from '../../services/auth';
 
 export function passwordsMatchValidator(control: AbstractControl): ValidationErrors | null {
@@ -17,7 +17,7 @@ export function passwordsMatchValidator(control: AbstractControl): ValidationErr
 
 @Component({
   selector: 'app-register',
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterModule, ReactiveFormsModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
@@ -25,14 +25,36 @@ export class Register {
   private auth = inject(Auth);
 
   registerForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     confirmPassword: new FormControl('', [Validators.required])
   }, { validators: passwordsMatchValidator }); 
 
+  serverError = signal<string | null>(null);
+
   onSubmit() {
     if (this.registerForm.valid) {
-      const { confirmPassword, ...userData } = this.registerForm.value;
+      this.serverError.set(null); // Czyszczenie błędów przed wysłaniem
+      const userData = {
+        firstName: this.registerForm.value.firstName,
+        lastName: this.registerForm.value.lastName,
+        email: this.registerForm.value.email,
+        password: this.registerForm.value.password
+      };
+
+      // --- SYMULACJA WALIDACJI DOMENY ---
+      // Docelowo ten błąd wyrzuci backend, my go tylko przechwycimy.
+      // Teraz symulujemy to na frontendzie, zakładając, że dozwolona domena to "nazwafirmy.pl"
+      const email = userData.email || '';
+      const domain = email.split('@')[1];
+
+      if (domain !== 'nazwafirmy.pl') {
+        this.serverError.set('Rejestracja dozwolona tylko dla autoryzowanych domen firmowych (np. @nazwafirmy.pl).');
+        return;
+      }
+      // ----------------------------------
 
       this.auth.register(userData);
     }
